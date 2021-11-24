ef347c35d0482fd14ab920edc610193c3a62b485
verger <sophie.verger2@gmail.com>
2021-11-25 00:09:35 +0100

    add session set id + show last bird seen

--- angrybirds/src/Controller/BirdController.php
+++ angrybirds/src/Controller/BirdController.php
@@ -9,23 +9,29 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BirdController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(BirdRepository $birdRepository): Response
+    public function index(BirdRepository $birdRepository, SessionInterface $session): Response
     {
 
+        $lastBirdId = $session->get('lastBirdId');
+
+        
         return $this->render('bird/index.html.twig', [
             'birds' => $birdRepository->findAll(),
+            'lastBird' => $birdRepository->find($lastBirdId),
         ]);
     }
 
     #[Route('/{id}', name: 'bird_single')]
-    public function single(Bird $bird, BirdRepository $birdRepository)
+    public function single(Bird $bird, BirdRepository $birdRepository, SessionInterface $session)
     {
-     
+        $session->set("lastBirdId", $bird->getId());
+        
         return $this->render('bird/single.html.twig', [
             'bird' => $birdRepository->find($bird->getId()),
         ]);
