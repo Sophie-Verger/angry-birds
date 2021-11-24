929ad4d028b322c5a858acd94cfecbc0b310ce58
verger <sophie.verger2@gmail.com>
2021-11-24 19:20:49 +0100

    create database + entity

--- angrybirds/src/Controller/BirdController.php
+++ angrybirds/src/Controller/BirdController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+
 use App\Model\BirdModel;
+use App\Repository\BirdRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,11 +12,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class BirdController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(BirdModel $birdModel): Response
+    public function index(BirdRepository $birdRepository): Response
     {
 
         return $this->render('bird/index.html.twig', [
-            'birds' => $birdModel->getBirds(),
+            'birds' => $birdRepository->findAll(),
         ]);
     }
 }
